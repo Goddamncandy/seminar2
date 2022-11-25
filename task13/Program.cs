c66799021a69ed283f566116220a8f9248857a36
Vera <akane@inbox.ru>
2022-11-25 11:42:56 +0400

    Добавлено задание 13

---
+++ task13/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 100)
+    Console.WriteLine("Третьей цифры нет");
+else if (a > 99 && a < 1000)
+    Console.WriteLine(a % 10);
